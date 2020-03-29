d1d911a34361b08b95285a90e58ade58901ace64
Skyler Dyess <dyesssp@gmail.com>
2020-03-28 21:50:10 -0500

    Add godeps

--- main.go
+++ main.go
@@ -6,10 +6,19 @@ import (
 	"net/http"
 )
 
+func getAllBlogPosts(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getBlogPost(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 
 	router := mux.NewRouter()
-
+	router.HandleFunc("/blog/posts", getAllBlogPosts).Methods("GET")
+	router.HandleFunc("/blog/posts/{id}", getBlogPost).Methods("GET")
 
 	err := http.ListenAndServe(":8000", router)
 	if err != nil {
